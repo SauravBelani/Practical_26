b4a8ab74d217c66085846e8650ab60a703f7bb2b
SF-CPU-251\saurav <saurav.b@simformsolutions.com>
2022-04-06 11:23:55 +0530

    Add project files.

---
+++ Practical_15/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practical_15.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        public HomeController(ILogger<HomeController> logger)
+        {
+            Logger = logger;
+        }
+
+        public ILogger<HomeController> Logger { get; }
+
+        [HttpGet]
+        public string Index()
+        {
+            Logger.LogInformation(">>>>>>>Called index action<<<<<<<");
+            return "Hello World!!!";
+        }
+    }
+}
